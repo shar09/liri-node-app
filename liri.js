3e57220d7e6070fe1794812c28f65219e5bb982b
shar09 <sharathtelu9@gmail.com>
2019-06-25 23:00:52 -0500

    movie api working

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ var moment = require("moment");
 var option = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
-function bands(artist) {
+function displayBands(artist) {
 
 //var artist = input;
 
@@ -61,24 +61,63 @@ function displaySong(song) {
     })
 }
 
+function displayMovie(movie) {
+    //console.log(movie);
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie+ "&y=&plot=short&tomatoes=true&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function(response) {
+          //console.log(response);  
+          console.log("Title: " +response.data.Title);
+          console.log("Release Year: " +response.data.Year);
+          console.log("IMDB Rating: " +response.data.imdbRating);
+          console.log("Rotten Tomatoes Rating: " +response.data.tomatoRating);
+          console.log("Language: " +response.data.Language);
+          console.log("Country: " +response.data.Country);
+          console.log("Plot: " +response.data.Plot);
+          console.log("Actors: " +response.data.Actors);
+        })
+        .catch(function(error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+      
+}
+
 function run(option, input) {
 switch(option) {
     case "concert-this":
-    bands(input);
+    displayBands(input);
     break;
 
     case "spotify-this-song":
     displaySong(input);
     break;
 
-//  case "movie-this":
-//  break;
+    case "movie-this":
+    displayMovie(input);
+    break;
 
 //  case "do-what-it-says":
 //  break;
     
     default:
-        console.log("Command not found");
+    console.log("Command not found");
 }
 }
 
